f55097d531f106732691ddeb1c6a4ed15fae3d95
Pavel <lmr_16@mail.ru>
2022-12-19 18:23:01 +0300

    start project

--- GuessTheNumber/ContentView.swift
+++ GuessTheNumber/ContentView.swift
@@ -8,9 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var value: Float = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 20) {
+            Text("Подвиньте слайдер как можно ближе к: \(Int(value))")
+                .frame(alignment: .leading)
+                .font(.system(size: 15))
+                
+            HStack {
+                Text("0")
+                Slider(value: $value, in: 0...100)
+                Text("100")
+            }
+            Button("Проверь меня!", action: {})
+            Button("Начать заново", action: {})
+        }
+        .padding()
     }
 }
 
